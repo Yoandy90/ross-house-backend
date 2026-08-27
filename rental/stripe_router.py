4b4661fcaf5af0db59be7501fee5a1e6c9801292
Yoandy90 <yoandyross@gmail.com>
2026-08-27 14:04:57 -0500

    feat(payments): register reconciliation workflow dashboard

--- rental/stripe_router.py
+++ rental/stripe_router.py
@@ -19,6 +19,7 @@ from rental.stripe_pkg.autopay_router import router as _autopay_router
 from rental.stripe_pkg.reconciliation_queue_router import router as _reconciliation_queue_router
 from rental.stripe_pkg.reconciliation_resolution_router import router as _reconciliation_resolution_router
 from rental.stripe_pkg.reconciliation_execution_router import router as _reconciliation_execution_router
+from rental.stripe_pkg.reconciliation_workflow_router import router as _reconciliation_workflow_router
 
 from rental.stripe_pkg.helpers import (  # noqa: F401
     _get_stripe_config,
@@ -46,3 +47,4 @@ router.include_router(_autopay_router)
 router.include_router(_reconciliation_queue_router)
 router.include_router(_reconciliation_resolution_router)
 router.include_router(_reconciliation_execution_router)
+router.include_router(_reconciliation_workflow_router)
